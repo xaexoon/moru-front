b74197fbc8446ab57a5cee5d0f18012d73e6b028
xaexoon <xaexoon07@gmail.com>
2025-10-22 02:09:12 +0900

    코드 정리

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>로그인 페이지 입니다</div>
+      </header>
+      <body>로그인 Content</body>
+    </div>
+  );
+}
+
+export default Login;
