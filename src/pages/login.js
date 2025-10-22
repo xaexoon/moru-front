4ef2de9f82be55b870f7f9040171666b51c8acb5
kyunghye <kyoung00828@gmail.com>
2025-10-22 22:28:55 +0900

    feat: 로그인창 구현

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import "./login_kim.css";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>로그인 페이지 입니다</div>
-      </header>
+    <div class="App">
+      <header class="App-header"></header>
+      <div class="container">
+        <div class="loginContainer">
+          <div class="icon"></div>
+          <p class="loginText"> Welcom back</p>
+          <p class="descriptionText">Sign in to continue</p>
+          <form class="inputContainer">
+            <input
+              class="loginInput"
+              type="email"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              class="loginInput"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button class="loginBtn" type="submit">
+              Login
+            </button>
+          </form>
+          <p class="socialText">Or sign in with</p>
+        </div>
+      </div>
       <body>로그인 Content</body>
     </div>
   );
