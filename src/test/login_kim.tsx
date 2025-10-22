f5c25347464a273f86def8148e1907231f304ac5
kyunghye <kyoung00828@gmail.com>
2025-10-22 22:41:32 +0900

    Rename: 파일 위치 변경

---
+++ src/test/login_kim.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./login_kim.css";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <div className="container">
+        <div className="loginContainer">
+          <div className="icon"></div>
+          <p className="loginText"> Welcom back</p>
+          <p className="descriptionText">Sign in to continue</p>
+          <form className="inputContainer">
+            <input
+              className="loginInput"
+              type="email"
+              placeholder="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="loginBtn" type="submit">
+              Login
+            </button>
+          </form>
+          <p className="socialText">Or sign in with</p>
+        </div>
+      </div>
+      <body>로그인 Content</body>
+    </div>
+  );
+}
+
+export default Login;
