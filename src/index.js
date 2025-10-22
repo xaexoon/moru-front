34a2591533ab8ead4c23fad5068a93dfd2bfcb96
xaexoon <xaexoon07@gmail.com>
2025-10-23 01:30:47 +0900

    파일 구조 변경 및 메인화면 틀 형성 작업중

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-undef */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Login from "./test/login_kim.tsx";
+// import Login from "./test/login_kim.tsx";
+import Main from "./pages/main/main.js";
 // import Login from "./pages/login.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Main />
   </React.StrictMode>
 );
