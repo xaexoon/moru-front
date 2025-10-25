bf7a7655defe906ddca7a084f1c38a78cf3f9199
kyunghye <kyoung00828@gmail.com>
2025-10-25 19:04:55 +0900

    feat: 인벤토리 구현중

--- src/pages/inventory/Inventory.js
+++ src/pages/inventory/Inventory.js
@@ -1,7 +1,17 @@
 import "./Inventory.css";
 
 function Inventory() {
-  return <></>;
+  return (
+    <div className="main_Container">
+      <div className="inventory_left">
+        <div className="inventory_top">
+          <div>내 인벤토리</div>
+        </div>
+        <div className="inventory_bottom"></div>
+      </div>
+      <div className="inventory_right"></div>
+    </div>
+  );
 }
 
 export default Inventory;
