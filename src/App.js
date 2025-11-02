38c86aec4f93fe1156ca3ed7a02ce75b0fb72955
kyunghye <kyoung00828@gmail.com>
2025-11-02 21:31:00 +0900

    fix: 인벤토리 ui 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import KimLogin from "./test/login_kim";
-import SongLogin from "./test/login_song";
 import Login from "./pages/login/login";
 import Inventory from "./pages/inventory/Inventory";
 import Layout from "./components/Layout";
@@ -81,8 +79,6 @@ function App() {
         />
 
         <Route path="/login" element={<Login />} />
-        <Route path="/kim/login" element={<KimLogin />} />
-        <Route path="/song/login" element={<SongLogin />} />
       </Routes>
     </BrowserRouter>
   );
